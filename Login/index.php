fc216f5446ae567c2ac4ec7d93a0a2caba3c0739
TOODDS <nogueiralimajoaopedro@gmail.com>
2025-05-24 11:48:14 -0300

    Index Atualizado

--- Login/index.php
+++ Login/index.php
@@ -14,9 +14,12 @@ include_once ('login.php');
 </head>
 <body>
 
-     <form action="" method="post">
+    <form action="" method="post">
 
         <div class = "login-container">
+              <div class="Logo-container">
+                    <img src="../css/imagens/logo_sem_fundo_azul.png" alt="Logo" class="Logo-img">
+                </div>
             <div class = "login-box">
                 <h2>Login</h2>
                 <input type="text" name = "email" placeholder = "Email">
@@ -25,17 +28,19 @@ include_once ('login.php');
                     Não tem uma conta? Crie uma aqui!
                 </p>
                 <button type ="submit" id = "botao-login" class="login-button">LOGIN</button>
-           
             </div>
+    </form>
             
             <div class = "welcome-box">
                 <h1><strong>Seja bem vindo</strong> de volta ao seu site de gestão de <br> pacientes!</h1>
                 <p>Faça o login para acessar sua conta</p>
                 <img src="../css/imagens/tela_login2.png" alt="Ilustração">
             </div>
+        </div>
+
             
-        </form>
-    </div>
+       
+       
     
 </body>
 </html>
