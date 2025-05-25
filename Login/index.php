74ff36ddf33dd219b975ad5ed4c6d7669237a574
TOODDS <nogueiralimajoaopedro@gmail.com>
2025-05-25 11:42:58 -0300

    index atualizado

--- Login/index.php
+++ Login/index.php
@@ -5,7 +5,7 @@ include_once ('login.php');
 
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,33 +14,44 @@ include_once ('login.php');
 </head>
 <body>
 
-    <form action="" method="post">
+    <form method="post">
 
-        <div class = "login-container">
-              <div class="Logo-container">
-                    <img src="../css/imagens/logo_sem_fundo_azul.png" alt="Logo" class="Logo-img">
+        <div id = "caixa-principal">
+                <div id ="Logo-container">
+                    <img src="../css/imagens/logo_sem_fundo_azul.png" alt="Logo" id="Logo-img">
                 </div>
-            <div class = "login-box">
+            <div id = "caixa-login">
                 <h2>Login</h2>
-                <input type="text" name = "email" placeholder = "Email">
-                <input type="password" name = "senha" placeholder = "Senha">
-                <p class = "signup-text">
+
+                <div class = "campo">
+                        <label for="email">Digite seu email:</label>
+                        <input type="text" name = "email" id = "email" placeholder = "Email">
+                </div>
+
+                <div class = "campo">
+                        <label for="senha">Digite sua senha:</label>
+                        <input type="password" name = "senha" id = "senha" placeholder = "Senha">
+                </div>
+
+                <p id = "texto-cadastro">
                     Não tem uma conta? Crie uma aqui!
                 </p>
-                <button type ="submit" id = "botao-login" class="login-button">LOGIN</button>
+
+                <button type ="submit" id ="botao-login">LOGIN</button>
             </div>
     </form>
             
-            <div class = "welcome-box">
-                <h1><strong>Seja bem vindo</strong> de volta ao seu site de gestão de <br> pacientes!</h1>
-                <p>Faça o login para acessar sua conta</p>
+            <div id = "caixa-secundaria">
+                <h1>
+                    <strong>Seja bem vindo</strong> de volta ao seu site de gestão de <br> pacientes!
+                </h1>
+                <p>
+                    Faça o login para acessar sua conta
+                </p>
+
                 <img src="../css/imagens/tela_login2.png" alt="Ilustração">
             </div>
         </div>
 
-            
-       
-       
-    
 </body>
 </html>
