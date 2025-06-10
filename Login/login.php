e500a79b88356c027c099c097e74bffa4e80572e
TOODDS <nogueiralimajoaopedro@gmail.com>
2025-06-10 13:30:58 -0300

    adicionado o prepare

--- Login/login.php
+++ Login/login.php
@@ -1,42 +1,29 @@
 <?php
-include_once ('conexao.php');
+include_once('conexao.php');
 
-if(isset($_POST['email']) && isset($_POST['senha'])){
-    if(empty($_POST['email'])) {
+if (isset($_POST['email']) && isset($_POST['senha'])) {
+    if (empty($_POST['email'])) {
         $error = "Preencha seu e-mail!";
-    }
-    else if(empty($_POST['senha'])) {
-        $error =   "Preencha sua senha!";
-    }
-    else {
+    } else if (empty($_POST['senha'])) {
+        $error =  "Preencha sua senha!";
+    } else {
         $email = $_POST['email'];
         $senha = trim($_POST['senha']);
+        $stmt = $conexao->prepare("SELECT * FROM usuario WHERE email_usuario = ?");
+        $stmt->execute([$email]);
 
-        $sql_code = "SELECT * FROM usuario WHERE email = '$email' AND senha = '$senha'";
-        try {
-                $sql_query = $conexao->query($sql_code);
-            } catch (PDOException $e) {
-                die("Falha na execução: " . $e->getMessage());
-            }
-
-        $quantidade = $sql_query->rowCount();
-
-        if ($quantidade == 1) {
-
-            $usuario = $sql_query->fetch(PDO::FETCH_ASSOC); 
-
-            if(!isset($_SESSION)){
+        if ($stmt->rowCount() == 1) {
+            $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
+            if (!isset($_SESSION)) {
                 session_start();
             }
-
-            $_SESSION['id'] = $usuario['id'];
-            $_SESSION['nome'] = $usuario['nome'];
-
-            header("Location: ../index.php");
-
+            password_verify($senha, $usuario['senha']);
+                $_SESSION['id'] = $usuario['usuario_id'];
+                $_SESSION['nome'] = $usuario['usuario_nome'];
+                header("Location: ../index.php");
+                exit();
         } else {
-            $error = "Falha ao logar! E-mail ou senha erradas";
+            $error = "Falha ao logar! E-mail ou senha errados.";
         }
     }
 }
-?>
