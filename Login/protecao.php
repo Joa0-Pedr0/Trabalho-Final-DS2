8b62a486b26c25f886f2a9db641ac5a53b8fb546
TOODDS <nogueiralimajoaopedro@gmail.com>
2025-06-10 13:35:23 -0300

    proteção atualizada

--- Login/protecao.php
+++ Login/protecao.php
@@ -1,10 +1,10 @@
 <?php
 
-if(!isset($_SESSION)) {
+if (!isset($_SESSION)) {
     session_start();
 }
 
-if(!isset($_SESSION['id'])) {
-    header ('Location: Login/formula.php');
+if (!isset($_SESSION['id'])) {
+    header('Location: Login/formula.php');
 }
 ?>
