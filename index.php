38ce519084a0bdbae8f4f5c8f040a47d63cc120e
TOODDS <nogueiralimajoaopedro@gmail.com>
2025-06-19 21:04:05 -0300

    adicionado o botão do crud de consultas

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ include_once 'Login/protecao.php';
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Painel de Consultas</title>
+  <title>Tela inicial</title>
   <link rel="stylesheet" href="css/tela_inicial.css" />
 </head>
 
@@ -16,11 +16,15 @@ include_once 'Login/protecao.php';
   <h1>Seja bem vindo a tela principal!</h1>
   <div id="link">
     <div id="link1">
-      <a href="Pacientes/formulario.php">Crud pacientes</a>
+      <a href="Pacientes/read_pacientes.php">Crud pacientes</a>
     </div>
     <div id="link2">
+      <a href="Consultas/read_consultas.php">Crud consultas</a>
+    </div>
+    <div id="link3">
       <a href="Login/logout.php">sair da conta</a>
     </div>
+
 
   </div>
 
