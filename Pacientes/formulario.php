6716e141bf903995fd5774d0608dae6f5b90b35b
TOODDS <nogueiralimajoaopedro@gmail.com>
2025-06-19 21:08:07 -0300

    adicionado a logo

--- Pacientes/formulario.php
+++ Pacientes/formulario.php
@@ -1,4 +1,5 @@
 <?php
+require_once ('../Login/protecao.php');
 include_once 'paciente_cadastro.php';
 ?>
 
@@ -9,30 +10,60 @@ include_once 'paciente_cadastro.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pacientes</title>
+    <link rel="stylesheet" href="../css/cadastro_pacientes.css">
 </head>
 
 <body>
-    <form method="post">
-        <?php
-        if (isset($error)) {
-            if (!empty($error)) {
-                echo $error;
+    <div id="Logo-container">
+        <img src="../css/imagens/logo_sem_fundo_azul.png" alt="Logo" id="Logo-img">
+        <p id="texto">PsiGestor</p>
+    </div>
+    <div class="form-container">
+        <form method="post" class="form-card">
+            <h2 class="form-title">Cadastrar paciente</h2>
+            <div id="erro">
+            <?php 
+            if(isset($error)) {
+                if(!empty($error)) {
+                    echo $error;
+                }
             }
-        }
+            ?>
+            </div>
+            <div class="form-group">
+                <label for="nome">Nome completo</label>
+                <input type="text" id="nome" name="nome" placeholder="Nome do paciente">
+            </div>
 
-        ?>
-        <label for="nome">Nome completo</label>
-        <input type="text" id="nome" name="nome" placeholder="Nome do paciente">
-        <label for="data">Data de Nascimento</label>
-        <input type="date" name="data" id="data">
-        <label for="procura">Motivo da procura</label>
-        <input type="text" name="procura" id="procura" placeholder="Motivo">
-        <label for="med">Usa medicação controlada?</label>
-        <input type="checkbox" name="med" id="med"> Sim
-        <label for="observacao">Observações adicionais</label>
-        <input type="text" id="observacao" name="observacao" placeholder="Observações">
-        <button>ENVIAR</button>
-    </form>
+            <div class="form-group">
+                <label for="data">Data de Nascimento</label>
+                <input type="date" name="data" id="data">
+            </div>
+
+            <div class="form-group">
+                <label for="procura">Motivo da procura</label>
+                <input type="text" name="procura" id="procura" placeholder="Motivo">
+            </div>
+
+            <div class="form-group checkbox-group">
+                <label for="med">Usa medicação controlada?</label>
+                <div class="checkbox-inline">
+                    <input type="checkbox" name="med" id="med">
+                    <label for="med">Sim</label>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="observacao">Observações adicionais</label>
+                <input type="text" id="observacao" name="observacao" placeholder="Observações">
+            </div>
+
+            <div class="button-group">
+                <button type="reset" onclick="window.location.href='read_pacientes.php'">Cancelar</button>
+                <button type="submit">Cadastrar</button>
+            </div>
+        </form>
+    </div>
 </body>
 
 </html>
